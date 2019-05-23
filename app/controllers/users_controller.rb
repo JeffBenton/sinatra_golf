6d7b85130a90fb044b71eb9b7016a4274d43c233
Jeff Benton <jeff@frogrock.net>
2019-05-23 18:02:59 -0400

    added user editing functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,25 @@ class UsersController < ApplicationController
     erb :'/users/show'
   end
 
+  post '/users/:id/edit' do
+    User.update(params[:id], username: params[:username], email: params[:email])
+    redirect "/users/#{params[:id]}"
+  end
+
+  post '/users/:id/edit_password' do
+    user = User.find(params[:id])
+    if user.authenticate(params[:current_password]) && params[:new_password] == params[:new_password_confirm]
+      User.update(params[:id], password: params[:new_password])
+      redirect "/users/#{params[:id]}"
+    else
+      redirect "/users/#{params[:id]}/edit"
+    end
+  end
+
+  get '/users/:id/edit' do
+    @user = User.find(params[:id])
+    @session = session
+    erb :'/users/edit'
+  end
+
 end
